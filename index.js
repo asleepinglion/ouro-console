fbbd6d50d40040f24e9823ab95a4057febe2c854
Aaron Storck <aaronstorck@gmail.com>
2015-04-17 15:27:34 -0400

    Refactored to use the new module system.

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ YSS'      YSSP~YSSY    S*S           YSSP  S*S    SSS  YSSY    YSS'
 
 */
 
-module.exports.Console = require('./modules/console/class');
+module.exports.Console = require('./lib/console/class');
